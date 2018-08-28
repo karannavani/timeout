76de79755b7ccc4e476a9b0f5cd026c227449b21
Karan Navani <kknavani@gmail.com>
2018-08-28 09:28:43 +0100

    added logic for minutes and seconds

--- app.js
+++ app.js
@@ -3,34 +3,37 @@ const button = document.querySelector('.button');
 const counterText = document.querySelector('.counterText');
 let minutes = 20;
 let seconds = 0;
-const secondsPlaceholder = 0;
-let clicked = false;
+let timerRunning = false;
 
-checkClicked();
 counterText.innerHTML = `${minutes}:0${seconds}`;
 
-function checkClicked() {
-  if (!clicked) {
+button.addEventListener('click', () => {
+  timerRunning = true;
+  minutes--;
+  seconds = 59;
+  console.log('clicked');
+  const secondsInterval = setInterval(() => {
+    checkSeconds();
+    seconds--;
+    // counterText.innerHTML = `${minutes}:${seconds}`;
+  }, 1000);
+
+  // const minutesInterval = setInterval(() => {
+  //   minutes--;
+  // }, 60000);
+  // setTimeout(() => {
+  //   alert('Take a break');
+  // }, 3000);
+});
+
+function checkSeconds() {
+  if (seconds === 0 && timerRunning) {
+    counterText.innerHTML = `${minutes}:00`;
+    seconds = 60;
+    minutes--;
+  } else if(seconds < 10) {
     counterText.innerHTML = `${minutes}:0${seconds}`;
   } else {
     counterText.innerHTML = `${minutes}:${seconds}`;
   }
 }
-
-button.addEventListener('click', () => {
-  clicked = true;
-  seconds = 59;
-  checkClicked();
-  console.log('clicked');
-  const secondsInterval = setInterval(() => {
-    seconds--;
-    counterText.innerHTML = `${minutes}:${seconds}`;
-  }, 1000);
-
-  const minutesInterval = setInterval(() => {
-    minutes--;
-  }, 60000);
-  // setTimeout(() => {
-  //   alert('Take a break');
-  // }, 3000);
-});
